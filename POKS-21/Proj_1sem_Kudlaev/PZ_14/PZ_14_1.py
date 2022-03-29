377e0008dba5ce1cc8b4e049f33a8ab007ce943e
dervvy <xtqzzzz@yandex.ru>
2022-03-29 23:36:23 +0300

    PZ_14

--- POKS-21/Proj_1sem_Kudlaev/PZ_14/PZ_14_1.py
+++ POKS-21/Proj_1sem_Kudlaev/PZ_14/PZ_14_1.py
@@ -3,10 +3,10 @@
 
 import re
 p = re.compile(r'1857')
-with open('Dostoevsky.txt', encoding='utf-8') as f:
+with open('Dostoevsky.txt', encoding='utf-8') as f: # открытие файла
     a = f.read()
-    b = re.findall(p, a)
-with open('1857.txt', 'w', encoding='utf-8') as f2:
+    b = re.findall(p, a) # поиск по файлу
+with open('1857.txt', 'w', encoding='utf-8') as f2: # создание нового файла
     f2.write(str(b[0]))
 
 h = re.compile(r'\n')
@@ -18,7 +18,7 @@ with open('Dostoevsky.txt', encoding='utf-8') as f:
 c = l.index('1857 год')
 d = l.index('1860–1866 гг.')
 with open('1857.txt', 'a', encoding='utf-8') as f2:
-        for i in l:
+        for i in l: # поиск границ блока
             if i == '1857 год':
                 for v in l[c+1:d]:
                     if v != '1860–1866 гг.':
