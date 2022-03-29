8a1e47543312c1f9504816b0bf7d1ddb9d1b07fa
dervvy <xtqzzzz@yandex.ru>
2022-03-29 23:21:41 +0300

    PZ 14

---
+++ POKS-21/Proj_1sem_Kudlaev/PZ_14/PZ_14_1.py
@@ -0,0 +1,25 @@
+# Из исходного текстового файла (Dostoevsky.txt) выбрать блок
+# информации за 1857 год и поместить ее в новый текстовый файл.
+
+import re
+p = re.compile(r'1857')
+with open('Dostoevsky.txt', encoding='utf-8') as f:
+    a = f.read()
+    b = re.findall(p, a)
+with open('1857.txt', 'w', encoding='utf-8') as f2:
+    f2.write(str(b[0]))
+
+h = re.compile(r'\n')
+
+with open('Dostoevsky.txt', encoding='utf-8') as f:
+    t = f.read()
+    l = re.split(h, t)
+
+c = l.index('1857 год')
+d = l.index('1860–1866 гг.')
+with open('1857.txt', 'a', encoding='utf-8') as f2:
+        for i in l:
+            if i == '1857 год':
+                for v in l[c+1:d]:
+                    if v != '1860–1866 гг.':
+                            f2.write(f'\n{v}')
